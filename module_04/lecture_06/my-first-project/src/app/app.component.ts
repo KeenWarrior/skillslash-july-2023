aceb8b7fa7cafc55236e410b9fe390bc43476000
keenwarrior <anujgargcse@gmail.com>
2023-11-25 15:53:55 +0530

    Adds material angular

--- module_04/lecture_06/my-first-project/src/app/app.component.ts
+++ module_04/lecture_06/my-first-project/src/app/app.component.ts
@@ -1,16 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  http: HttpClient;
+
+  username = ''; 
+
+  selectedDate: Date = new Date();
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
   title = 'Bingo';
 
   counter = 0;
 
   increment = () => {
     this.counter += 1;
-  }
+  };
+
+  user: any = {
+    name: 'Loading...',
+    avatar_url:
+      'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png',
+  };
+
+  getUser = () => {
+    this.http
+      .get('https://api.github.com/users/' + this.username)
+      .subscribe((data) => {
+        this.user = data;
+      });
+  };
 }
--- module_04/lecture_06/my-first-project/src/app/app.module.ts
+++ module_04/lecture_06/my-first-project/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from "@angular/material/core";
+
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    MatDatepickerModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
