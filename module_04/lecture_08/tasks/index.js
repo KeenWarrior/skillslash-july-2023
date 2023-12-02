333ceb49938d4a1bf5194bce219f329dbf9e28cd
keenwarrior <anujgargcse@gmail.com>
2023-12-02 16:28:35 +0530

    L 8

--- module_04/lecture_08/tasks/index.js
+++ module_04/lecture_08/tasks/index.js
@@ -1,21 +1,29 @@
-let startTime = new Date().getMilliseconds();
+const moment = require("moment");
+
+let startTime = moment();
+
+const times = [0, 1, 10];
+
+startTime.add(times[0], "hours");
+startTime.add(times[1], "minutes");
+startTime.add(times[2], "seconds");
+
+const targetTime = startTime.toDate().getTime();
 
 setInterval(() => {
-  let currentTime = new Date().getMilliseconds();
-  let delta = currentTime - startTime;
+  let currentTime = moment().toDate().getTime();
+  let delta = targetTime - currentTime;
   const hours = Math.floor(delta / (1000 * 60 * 60));
-    delta -= hours * 3600000;
+  delta = delta % (60 * 60 * 1000);
   const minutes = Math.floor(delta / (1000 * 60));
-  delta -= minutes * (60 * 1000);
-  const seconds = Math.floor(
-    (delta - hours * 3600000 - minutes * 60000) / 1000
-  );
+  delta = delta % (60 * 1000);
+  const seconds = Math.floor(delta / 1000);
   console.log(hours, minutes, seconds);
 }, 1000);
 
-setInterval(() => {
-  let s = "";
-  for (let i = 0; i < 100000000; i++) {
-    s += "a";
-  }
-}, 1000);
+// setInterval(() => {
+//   let s = "";
+//   for (let i = 0; i < 100000000; i++) {
+//     s += "a";
+//   }
+// }, 1000);
